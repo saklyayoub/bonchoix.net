0c30506b789081972062e48dcab4b3c44510f5fb
Sakly <saklyayoub@live.com>
2013-05-27 12:44:53 +0100

    vidage de base de dooner

--- include/config_serv.php
+++ include/config_serv.php
@@ -1,7 +1,7 @@
 <?php
-$serveur="sql300.t2h.pl";
-$utilisateur="t2h_11950776";
-$mot_de_passe="20666996";
-$base_de_donnees="t2h_11950776_bonchoix";
-$adress_du_site="http://www.phpproject.t2h.pl"; //sans / a la fin 
+$serveur="mysql5.000webhost.com ";
+$utilisateur="a9120684_bc";
+$mot_de_passe="20666996€";
+$base_de_donnees="a9120684_bc";
+$adress_du_site="http://bonchoix.comze.com"; //sans / a la fin 
 ?>
