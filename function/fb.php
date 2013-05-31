30f2d56f8f87e6ca03e309474a7122d0de02c340
Sakly Ayoub <saklyayoub@live.com>
2013-05-31 12:41:58 +0100

    maj e-bauche

--- function/fb.php
+++
@@ -1,9 +0,0 @@
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-like-box" id="fb" data-href="<?php echo $page_fb;?>?notif_t=fbpage_fan_invite" data-width="250" data-height="320" data-show-faces="true" data-stream="false" data-header="false"></div>
--- include/config_serv.php
+++
@@ -1,7 +0,0 @@
-<?php
-$serveur="mysql5.000webhost.com ";
-$utilisateur="a9120684_bc";
-$mot_de_passe="20666996€";
-$base_de_donnees="a9120684_bc";
-$adress_du_site="http://bonchoix.comze.com"; //sans / a la fin 
-?>
