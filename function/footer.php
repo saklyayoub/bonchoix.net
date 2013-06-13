64ea679cf8f9d4e768ca39fe48341f2b5066536d
Sakly Ayoub <saklyayoub@live.com>
2013-06-14 00:23:40 +0100

    Maj Footer Dynamiques

--- function/footer.php
+++ function/footer.php
@@ -1,35 +1,31 @@
 <div id="footer" class="g_box">
+	<div id="footer1">
+		<p class="footer_titre">Le <?php echo $nom_du_site ; ?>, votre annuaire des bijoux</p>
+	</div>
 
-<div id="footer1">
-
-<p class="footer_titre">Le bon choix, votre annuaire des bijoux</p>
-
-</div>
-
-<div id="footer2">
-<ul>
-<li class="footer_sous_titre">Accueil</li>
-<li class="footer_sous_titre">Notre Catalogue</li>
-<li class="footer_sous_titre">Mon panier</li>
-<li class="footer_sous_titre">Contactez-nous</li>
-</ul>
-</div>
-
-<div id="footer3">
-<ul>
-<li class="footer_sous_titre"><b>Contact</b></li>
-<li class="footer_sous_titre"><b>Web:</b>www.lebonchoix.com.tn</li>
-<li class="footer_sous_titre"><b>Email:</b>info@lebonchoix.tn</li>
-<li class="footer_sous_titre"><b>Tél:</b> 71 712 727</li>
-</ul>
-</div>
-
-
-<div id="footer4">
-<ul>
-<li class="footer_sous_titre"><b>Adresse:</b> 5 avenue habib bourguiba ariana</li>
-</ul>
-<br /><br />
-<p class="footer_sous_titre">Copyright by 2msa.com</p>
-</div>
+	<div id="footer2">
+		<ul>
+			<li class="footer_sous_titre"><a href="index.php">Accueil</a></li>
+			<li class="footer_sous_titre"><a href="catalogue.php">Notre Catalogue</a></li>
+			<li class="footer_sous_titre"><a href="panier.php">Mon panier</a></li>
+			<li class="footer_sous_titre"><a href="contact.php">Contactez-nous</a></li>
+		</ul>
+	</div>
+	<div id="footer3">
+		<ul>
+			<li class="footer_sous_titre"><b>Contact</b></li>
+			<li class="footer_sous_titre"><b>Web:</b><?php echo $adress_du_site ; ?></li>
+			<li class="footer_sous_titre"><b>Email:</b><?php echo $text_recherche ; ?></li>
+			<li class="footer_sous_titre"><b>Tél:</b> <?php echo $tel ; ?></li>
+		</ul>
+	</div>
+	<div id="footer4">
+		<ul>
+			<li class="footer_sous_titre"><?php echo $adresse ; ?></li>
+            <li>&nbsp;</li>
+            <li>&nbsp;</li>
+            <li>&nbsp;</li>
+            <li class="footer_sous_titre" style="width:300px;"><b>COPYRIGHT 2MSA.COM®</b></li>
+        </ul>
+	</div>
 </div>
